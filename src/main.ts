e736566b76fa43b92c24bf6848a5824a50987d50
Mehmet Emin Eker <mehmetemineker92@gmail.com>
2023-07-19 15:07:10 +0300

    primevue fix

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button';
 
-
 const app = createApp(App);
-app.mount('#app')
+
 app.use(PrimeVue)
 
 app.component('Button', Button)
+
+app.mount('#app')
 
 
 
