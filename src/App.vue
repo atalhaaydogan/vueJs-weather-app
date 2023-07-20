29c359b4b291d84a303ac3c352f740965df77b7f
atalhaaydogan <atalhaaydogan@gmail.com>
2023-07-20 10:22:59 +0300

    background and icon added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import axios from 'axios';
@@ -8,15 +7,76 @@ import axios from 'axios';
 const url = import.meta.env.VITE_WEATHER_API_URL;
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-console.log(url, apiKey)
-
 const searchInput = ref<string>();
 const city = ref<string>();
 const temp = ref<any>();
 const desc = ref<string>();
 const minmax = ref<any>();
 
+const getImageFromAPI = (city : string ) => {
+    fetch('https://api.teleport.org/api/urban_areas/slug:'+city+'/images/').then((resp)=>{
+        return resp.json();
+    }).then((data)=>{
+        console.log(data);
+        const imageUrl =  data.status === 404 ? '/img.jpg': data?.photos[0]?.image?.mobile;
+        console.log(imageUrl);
+        document.body.style.backgroundImage = "url('"+imageUrl +"')";
+    }).catch(
+        (err) => {
+            console.log(err);
+            document.body.style.backgroundImage = "url(/img.jpg)";
+        }
+    )
+}
+
+const getWeatherIcon = (description: string) => {
+
+  let iconClass = 'pi pi-question-circle';
+  switch(description) {
+  case'clear sky':
+  iconClass = 'pi pi-sun';
+  break;
+  case'few clouds':
+  iconClass = 'pi pi-cloud';
+  break;
+  case'scattered clouds':
+  iconClass = 'pi pi-cloud';
+  break;
+  case'broken clouds':
+  iconClass = 'pi pi-cloud';
+  break;
+  case'shower rain':
+  iconClass = 'pi pi-cloud-rain';
+  break;
+  case'rain':
+  iconClass = 'pi pi-cloud-rain';
+  break;
+  case'thunderstorm':
+  iconClass = 'pi pi-flash';
+  break;
+  case'snow':
+  iconClass = 'pi pi-snowflake';
+  break;
+  case'mist':
+  iconClass = 'pi pi-fog';
+  break;
+  case'smoke':
+  iconClass = 'pi pi-fog';
+  break;
+  case'haze':
+  iconClass = 'pi pi-fog';
+  break;
+  case'dust':
+  iconClass = 'pi pi-fog';
+  break;
+}
+
+  return iconClass;
+};
+
+
 const getWeather = () => {
+  getImageFromAPI(searchInput.value);
   axios
     .get(`${url}weather`, {
       params: {
@@ -35,25 +95,36 @@ const getWeather = () => {
         ' ℃ / ' +
         Math.round(data.main.temp_max) +
         ' ℃';
+
     })
     .catch(() => alert('Wrong City Name!'));
 };
 </script>
 
 <template>
-  <div class="weather-app" m-20>
+  <div class="weather-app">
     <div class="header">
-      <h1>Weather</h1>
-      <input v-model="searchInput" type="text" placeholder="Enter city" />
+      <h1 class="mb-3">Weather</h1>
+      <!-- <input v-model="searchInput" type="text" placeholder="Enter city" /> -->
+      <div>
+        <span class="p-input-icon-left mb-3">
+          <i class="pi pi-search" />
+          <InputText v-model="searchInput" placeholder="Enter City" />
+        </span>
+      </div>
+
       <!-- <button @click="getWeather">Search</button> -->
-      <Button label="Submit" />
+      <Button class="mt-1" type="button" label="Search" icon="pi pi-search" @click="getWeather" />
     </div>
 
     <div class="content">
       <div class="city">{{ city }}</div>
       <div class="temp">{{ temp }} </div>
-      <div class="desc">{{ desc }}</div>
-      <div class="minmax">{{ minmax }}</div>
+      <div class="desc">
+        <Icon :class="getWeatherIcon(desc)" />
+        {{ desc }}
+      </div>
+      
 
     </div>
   </div>
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import 'primeflex/primeflex.css';
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button';
+import InputText from "primevue/inputtext";
+
 
 const app = createApp(App);
 
 app.use(PrimeVue)
 
 app.component('Button', Button)
+app.component('InputText', InputText)
+
+
 
 app.mount('#app')
 
